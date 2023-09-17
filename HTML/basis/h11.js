ca0b6aa9a1ace1534fa200bb7d848696b0db5b67
zhengmianmian <zmianmian8@gmail.com>
2023-09-17 21:58:46 +1200

    simple drag and drop divs

---
+++ HTML/basis/h11.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".container");
+let source;
+container.ondragstart = e=>{
+    e.dataTransfer.effectAllowed = e.target.dataset.effect;//move copy
+    source = e.target;
+    //console.log('start', e.target)
+}
+container.ondragover = e=>{
+    e.preventDefault();
+    //console.log('over', e.target)
+}
+const clearDropStyle = ()=>{
+    document.querySelectorAll('.drop-over').forEach((node)=>{
+        node.classList.remove('drop-over')
+    })
+};
+function getDropNode(node){
+    while(node){
+        if(node.dataset && node.dataset.drop){
+            return node;
+        }
+        node = node.parentNode;
+    }
+}
+container.ondragenter = e=>{
+    clearDropStyle();
+    const dropNode = getDropNode( e.target);
+    if (dropNode && dropNode.dataset.drop===e.dataTransfer.effectAllowed) {
+        dropNode.classList.add('drop-over');
+    } 
+}
+container.ondrop = e=>{
+    const dropNode = getDropNode(e.target);
+    const cloned = source.cloneNode(true);
+    if (dropNode && dropNode.dataset.drop===e.dataTransfer.effectAllowed){
+        dropNode.innerHTML = ""
+        dropNode.appendChild(cloned);
+        clearDropStyle();
+    }
+    
+}
