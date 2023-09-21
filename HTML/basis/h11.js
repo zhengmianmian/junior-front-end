0d140614a0970da0f7548351f2db492b34006365
zhengmianmian <zmianmian8@gmail.com>
2023-09-21 21:42:32 +1200

    div follows mouse

--- HTML/basis/h11.js
+++ HTML/basis/h11.js
@@ -1,14 +1,32 @@
 const container = document.querySelector(".container");
+const boxContainer = document.querySelector(".box-container");
+const box = document.querySelector(".box");
 let source;
-container.ondragstart = e=>{
+const handleDragStart = (e)=>{
     e.dataTransfer.effectAllowed = e.target.dataset.effect;//move copy
     source = e.target;
-    console.log('start', e.dataTransfer)
+    console.log('start')
 }
+const handleDragEnd = (e)=>{
+    let x,y;
+    x = e.offsetX;
+    y = e.offsetY;
+    console.log("x is ",x, "y is ",y);
+}
+
+
+container.ondragstart = handleDragStart;
 container.ondragover = e=>{
     e.preventDefault();
     //console.log('over', e.target)
 }
+
+
+boxContainer.ondragstart = handleDragStart;
+boxContainer.ondragend = handleDragEnd;
+
+
+
 const clearDropStyle = ()=>{
     document.querySelectorAll('.drop-over').forEach((node)=>{
         node.classList.remove('drop-over')
@@ -39,5 +57,27 @@ container.ondrop = e=>{
         dropNode.appendChild(cloned);
         clearDropStyle();
     }
-    
 }
+
+window.onload = function(){
+    document.onmousemove = (e)=>{
+        e = e || window.event;
+        var left = e.pageX + 2;
+        var top = e.pageY + 2;
+        box.style.left = left+"px";
+        box.style.top = top+"px";
+    }
+}
+// const box2 = document.getElementById("box2");
+// box2.onmousedown = ()=>{
+//     document.onmousemove=(e)=>{
+//         e = e || window.event;
+//         var left = e.pageX;
+//         var top = e.pageY;
+//         box2.style.left = left+"px";
+//         box2.style.top = top+"px";
+//     }
+//     box2.onmouseup = ()=>{
+//         document.onmousemove = null;
+//     }
+// }
